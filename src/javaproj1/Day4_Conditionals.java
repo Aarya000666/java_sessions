88f141997b426715038bb18464955415c31804f3
tani p <aaryaaarya2006@gmail.com>
2025-01-06 11:39:36 +0530

    first commit

---
+++ src/javaproj1/Day4_Conditionals.java
@@ -0,0 +1,16 @@
+package javaproj1;
+
+public class Day4_Conditionals {
+    public static void main(String[] args) {
+        
+        // Conditional Statements
+        // 1. If-Else Statement 
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+            } else {
+                System.out.println("x is less than or equal to 5");
+                }
+    }
+}
+
