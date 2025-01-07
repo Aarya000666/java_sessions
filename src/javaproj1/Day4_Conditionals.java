ac9b21c736a8c342e037b7cd7d72836dc320d6af
tani p <aaryaaarya2006@gmail.com>
2025-01-07 09:20:32 +0530

    changes

--- src/javaproj1/Day4_Conditionals.java
+++ src/javaproj1/Day4_Conditionals.java
@@ -2,15 +2,58 @@ package javaproj1;
 
 public class Day4_Conditionals {
     public static void main(String[] args) {
+        /*
+         When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
+
+
+cigarParty(30, false) → false
+cigarParty(50, false) → true
+cigarParty(70, true) → true
+         */
+//         //public int caughtSpeeding(int speed, boolean isBirthday) {
+//     if (isBirthday) {
+//         if (speed - 5 <= 60) {
+//             return 0;
+//         } else if (speed - 5 >= 61 && speed - 5 <= 80) {
+//             return 1;
+//         } else{
+//             return 2;
+//         }
+//     } else {
+//         if (speed <= 60) {
+//             return 0;
+//         } else if (speed >= 61 && speed <= 80) {
+//             return 1;
+//         } else  {
+//             return 2;
+//         }
+//     }
+  
+// }
+
+// public int caughtSpeeding(int speed, boolean isBirthday) {
+//     if (isBirthday) {
+//         if (speed - 5 <= 60) {
+//             return 0;
+//         } else if (speed - 5 >= 61 && speed - 5 <= 80) {
+//             return 1;
+//         } else{
+//             return 2;
+//         }
+//     } else {
+//         if (speed <= 60) {
+//             return 0;
+//         } else if (speed >= 61 && speed <= 80) {
+//             return 1;
+//         } else  {
+//             return 2;
+//         }
+//     }
+  
+// }
+
         
-        // Conditional Statements
-        // 1. If-Else Statement 
-        int x = 10;
-        if (x > 5) {
-            System.out.println("x is greater than 5");
-            } else {
-                System.out.println("x is less than or equal to 5");
-                }
+
     }
 }
 
